87530a28f802bc93c7e4953cf4a308aedc178216
fllprbt <pgiouvanelis@gmail.com>
2018-09-22 20:00:16 +0200

    1. Add an edited version of OpenWeather's cities.json to project, interface the searchbar's term with the cities, fix lint-stage heap memory error with getJson on the city list instead of bundling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import '../build/styles/App.css';
 import { Row, Col, Container } from 'reactstrap';
+import _ from 'lodash';
+import $ from 'jquery';
 import SearchBar from './Searchbar';
 import ResultNav from './Result/ResultNav';
 
+/* eslint react/no-unused-state: 0 */
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    $.getJSON('/json/cities.min.json', json => {
+      this.cities = json;
+    });
+
+    this.state = {
+      city: '',
+      matches: [],
+    };
+
+    this.handleSearch = _.debounce(this.handleSearch.bind(this), 400);
+  }
+
+  handleSearch(city) {
+    this.setState({
+      city,
+      matches: this.cities.filter(item =>
+        item.name.toLowerCase().includes(city)
+      ),
+    });
   }
 
   render() {
@@ -17,7 +39,7 @@ class App extends React.Component {
           <h1 className="app__title">React Forecast</h1>
           <Row>
             <Col>
-              <SearchBar />
+              <SearchBar handleSearch={this.handleSearch} />
               <ResultNav />
             </Col>
           </Row>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,7 +3,14 @@ import { Input } from 'reactstrap';
 
 /* eslint react/prop-types: 0 */
 const Searchbar = ({ handleSearch }) => (
-  <Input type="search" placeholder="City Name" onChange={handleSearch} />
+  <Input
+    type="search"
+    placeholder="City / Region"
+    onChange={evt => {
+      handleSearch(evt.target.value.toLowerCase());
+    }}
+    autoFocus
+  />
 );
 
 export default Searchbar;
